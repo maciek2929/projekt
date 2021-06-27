db1567dc00c69a8397154c1200b1dd11f2012251
Maciek <maciek29.opozda@gmail.com>
2021-06-27 14:48:17 +0200

    initial Commmit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {NG_VALIDATORS} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+
+  myData: any = {
+    ax: 'Type your hex',
+    bx: 'Type your hex',
+    cx: 'Type your hex',
+    dx: 'Type your hex',
+  }
+
+  hexPattern = new RegExp(/[abc] +/g);
+
+
+  onMove(moveFrom: string, moveTo: string) {
+    this.myData[moveTo] = moveFrom
+  }
+
+  onXchg(xchg1: any, xchg2: any) {
+    const firstVal = xchg1.value;
+    const secondVal = xchg2.value;
+    this.myData[xchg1.options[xchg1.selectedIndex].text] = secondVal;
+    this.myData[xchg2.options[xchg2.selectedIndex].text] = firstVal;
+  }
+
+  onClear() {
+    this.myData = {
+      ax: '',
+      bx: '',
+      cx: '',
+      dx: '',
+    }
+  }
+
+}
